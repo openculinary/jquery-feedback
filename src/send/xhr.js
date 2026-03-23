8101e257eea51e2b5819e1041cf17446fece2271
James Addison <james@reciperadar.com>
2026-03-23 20:50:57 +0000

    jquery: migrate XMLHttpRequest to `jQuery.post`

--- src/send/xhr.js
+++ src/send/xhr.js
@@ -1,24 +1,14 @@
-window.Feedback.XHR = function( url ) {
-    
-    this.xhr = new XMLHttpRequest();
+window.Feedback.XHR = function(url) {
     this.url = url;
-
 };
 
 window.Feedback.XHR.prototype = new window.Feedback.Send();
 
-window.Feedback.XHR.prototype.send = function( data, callback ) {
-    
-    var xhr = this.xhr;
-    
-    xhr.onreadystatechange = function() {
-        if( xhr.readyState == 4 ){
-            callback( (xhr.status === 200) );
-        }
-    };
-    
-    xhr.open( "POST", this.url, true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send( "data=" + encodeURIComponent( window.JSON.stringify( data ) ) );
-     
+window.Feedback.XHR.prototype.send = function(data, callback) {
+    $.post({
+        url: this.url,
+        data: {data: JSON.stringify(data)},
+        success: function() { callback(true) },
+        error: function() { callback(false) }
+    });
 };
