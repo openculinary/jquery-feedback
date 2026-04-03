d1e15159d56b8ef9770e3818be429d993a2983f8
James Addison <james@reciperadar.com>
2026-04-03 16:11:40 +0100

    Form: allow browser-info collection to be configurable

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,5 +1,5 @@
 class Form extends Page {
-  constructor(elements) {
+  constructor(elements, browserInfo) {
     super();
     this.elements = elements || [
       {
@@ -9,8 +9,42 @@ class Form extends Page {
         required: false,
       },
     ];
+    this.browserInfo = browserInfo || this.defaultBrowserInfo();
 
     this.dom = $("<div />");
+  }
+
+  static defaultBrowserInfo() {
+    return {
+      url: true,
+      timeOpened: true,
+      timezone: true,
+      pageon: true,
+      referrer: true,
+      previousSites: true,
+      browserName: true,
+      browserEngine: true,
+      browserVersion1a: true,
+      browserVersion1b: true,
+      browserLanguage: true,
+      browserOnline: true,
+      browserPlatform: true,
+      javaEnabled: true,
+      dataCookiesEnabled: true,
+      dataCookies1: true,
+      dataCookies2: true,
+      dataStorage: true,
+      sizeScreenW: true,
+      sizeScreenH: true,
+      sizeDocW: true,
+      sizeDocH: true,
+      sizeInW: true,
+      sizeInH: true,
+      sizeAvailW: true,
+      sizeAvailH: true,
+      scrColorDepth: true,
+      scrPixelDepth: true,
+    };
   }
 
   render() {
@@ -53,34 +87,42 @@ class Form extends Page {
       data[item.name] = item.element.val();
     });
 
-    data.url = window.location.href;
-    data.timeOpened = new Date();
-    data.timezone = new Date().getTimezoneOffset() / 60;
-    data.pageon = window.location.pathname;
-    data.referrer = document.referrer;
-    data.previousSites = history.length;
-    data.browserName = navigator.appName;
-    data.browserEngine = navigator.product;
-    data.browserVersion1a = navigator.appVersion;
-    data.browserVersion1b = navigator.userAgent;
-    data.browserLanguage = navigator.language;
-    data.browserOnline = navigator.onLine;
-    data.browserPlatform = navigator.platform;
-    data.javaEnabled = navigator.javaEnabled();
-    data.dataCookiesEnabled = navigator.cookieEnabled;
-    data.dataCookies1 = document.cookie;
-    data.dataCookies2 = decodeURIComponent(document.cookie.split(";"));
-    data.dataStorage = localStorage;
-    data.sizeScreenW = screen.width;
-    data.sizeScreenH = screen.height;
-    data.sizeDocW = document.width;
-    data.sizeDocH = document.height;
-    data.sizeInW = innerWidth;
-    data.sizeInH = innerHeight;
-    data.sizeAvailW = screen.availWidth;
-    data.sizeAvailH = screen.availHeight;
-    data.scrColorDepth = screen.colorDepth;
-    data.scrPixelDepth = screen.pixelDepth;
+    if (this.browserInfo.url) data.url = window.location.href;
+    if (this.browserInfo.timeOpened) data.timeOpened = new Date();
+    if (this.browserInfo.timezone)
+      data.timezone = new Date().getTimezoneOffset() / 60;
+    if (this.browserInfo.pageon) data.pageon = window.location.pathname;
+    if (this.browserInfo.referrer) data.referrer = document.referrer;
+    if (this.browserInfo.previousSites) data.previousSites = history.length;
+    if (this.browserInfo.browserName) data.browserName = navigator.appName;
+    if (this.browserInfo.browserEngine) data.browserEngine = navigator.product;
+    if (this.browserInfo.browserVersion1a)
+      data.browserVersion1a = navigator.appVersion;
+    if (this.browserInfo.browserVersion1b)
+      data.browserVersion1b = navigator.userAgent;
+    if (this.browserInfo.browserLanguage)
+      data.browserLanguage = navigator.language;
+    if (this.browserInfo.browserOnline) data.browserOnline = navigator.onLine;
+    if (this.browserInfo.browserPlatform)
+      data.browserPlatform = navigator.platform;
+    if (this.browserInfo.javaEnabled)
+      data.javaEnabled = navigator.javaEnabled();
+    if (this.browserInfo.dataCookiesEnabled)
+      data.dataCookiesEnabled = navigator.cookieEnabled;
+    if (this.browserInfo.dataCookies1) data.dataCookies1 = document.cookie;
+    if (this.browserInfo.dataCookies2)
+      data.dataCookies2 = decodeURIComponent(document.cookie.split(";"));
+    if (this.browserInfo.dataStorage) data.dataStorage = localStorage;
+    if (this.browserInfo.sizeScreenW) data.sizeScreenW = screen.width;
+    if (this.browserInfo.sizeScreenH) data.sizeScreenH = screen.height;
+    if (this.browserInfo.sizeDocW) data.sizeDocW = document.width;
+    if (this.browserInfo.sizeDocH) data.sizeDocH = document.height;
+    if (this.browserInfo.sizeInW) data.sizeInW = innerWidth;
+    if (this.browserInfo.sizeInH) data.sizeInH = innerHeight;
+    if (this.browserInfo.sizeAvailW) data.sizeAvailW = screen.availWidth;
+    if (this.browserInfo.sizeAvailH) data.sizeAvailH = screen.availHeight;
+    if (this.browserInfo.scrColorDepth) data.scrColorDepth = screen.colorDepth;
+    if (this.browserInfo.scrPixelDepth) data.scrPixelDepth = screen.pixelDepth;
 
     // cache and return data
     return (this._data = data);
